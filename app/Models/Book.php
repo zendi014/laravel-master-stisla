e30f65eacf4e6a95630531ce8013fbedd7ff019c
BTI UMB <bti.umb@mercubuana.ac.id>
2023-06-03 12:50:02 +0700

    Update Book.php

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use SoftDeletes;
-use UuidTrait;
+use App\Models\Concerns\UuidTrait;
 
 class Book extends Model
 {
